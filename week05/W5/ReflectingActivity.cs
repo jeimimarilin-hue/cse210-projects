4c6889df605cb0363af44e28fd6cf3732753fee8
jeimimarilin-hue <jeimimarilin@gmail.com>
2026-06-05 13:30:26 -0400

    Add files via upload

---
+++ week05/W5/ReflectingActivity.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+
+public class ReflectingActivity : Activity
+{
+    private List<string> _prompts = new List<string>
+    {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless."
+    };
+
+    private List<string> _questions = new List<string>
+    {
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times?",
+        "What is your favorite thing about this experience?",
+        "What did you learn about yourself through this experience?",
+        "How can you keep this experience in mind in the future?"
+    };
+
+    public ReflectingActivity() : base("Reflecting Activity", 
+        "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
+    {
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+
+        string prompt = GetRandomPrompt();
+        Console.WriteLine("Consider the following prompt:\n");
+        Console.WriteLine($"--- {prompt} --- \n");
+        Console.WriteLine("When you have something in mind, press enter to continue.");
+        Console.ReadLine();
+
+        Console.WriteLine("Now ponder on each of the following questions as they relate to this experience.");
+        Console.Write("You may begin in: ");
+        ShowCountDown(5);
+        Console.Clear();
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            string question = GetRandomQuestion();
+            Console.Write($"> {question} ");
+            ShowSpinner(10);
+            Console.WriteLine();
+        }
+
+        DisplayEndingMessage();
+    }
+
+    private string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+
+    private string GetRandomQuestion()
+    {
+        Random random = new Random();
+        int index = random.Next(_questions.Count);
+        return _questions[index];
+    }
+}
