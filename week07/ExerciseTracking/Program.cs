121544389b6e5b598553bf657abf677b656a1811
jeimimarilin-hue <jeimimarilin@gmail.com>
2026-06-06 19:46:11 -0400

    Add files via upload

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracking
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            List<Activity> activities = new List<Activity>();
+
+            activities.Add(new Running("03 Nov 2022", 30, 3.0));
+            activities.Add(new Cycling("04 Nov 2022", 45, 15.0));
+            activities.Add(new Swimming("05 Nov 2022", 20, 20));
+
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+        }
     }
 }
