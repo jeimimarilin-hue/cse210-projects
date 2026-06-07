d241b64fb89c246b541336f30631f6a6cea70e5f
jeimimarilin-hue <jeimimarilin@gmail.com>
2026-06-07 10:12:10 -0400

    Add files via upload

---
+++ week05/ListingActivity.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+
+    public ListingActivity() : base("Listing Activity", 
+        "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
+    {
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+
+        Random random = new Random();
+        string prompt = _prompts[random.Next(_prompts.Count)];
+
+        Console.WriteLine("List as many responses you can to the following prompt:");
+        Console.WriteLine($"--- {prompt} ---");
+        Console.Write("You may begin in: ");
+        ShowCountDown(5);
+        Console.WriteLine();
+
+        _count = 0;
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            string input = Console.ReadLine();
+            if (!string.IsNullOrEmpty(input))
+            {
+                _count++;
+            }
+        }
+
+        Console.WriteLine($"You listed {_count} items!\n");
+        DisplayEndingMessage();
+    }
+}
---
+++ week05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string choice = "";
+
+        while (choice != "4")
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflecting activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                BreathingActivity breathing = new BreathingActivity();
+                breathing.Run();
+            }
+            else if (choice == "2")
+            {
+                ReflectingActivity reflecting = new ReflectingActivity();
+                reflecting.Run();
+            }
+            else if (choice == "3")
+            {
+                ListingActivity listing = new ListingActivity();
+                listing.Run();
+            }
+            else if (choice != "4")
+            {
+                Console.WriteLine("Invalid option. Press Enter to try again.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
